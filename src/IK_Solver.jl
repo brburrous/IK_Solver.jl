49bbe3a7060ca2b4c0ebbd479c8a3bceafeb32eb
Brian Burrous <brianon007@gmail.com>
2022-12-30 14:58:30 -0800

    added default exports

--- src/IK_Solver.jl
+++ src/IK_Solver.jl
@@ -9,14 +9,13 @@ include("Equation.jl")
 include("HelperFunctions.jl")
 include("DisplayEq.jl")
 include("Rotation.jl")
-foo(x) = print("Hello $x")
 
 greet() = print("Hello World!")
 
-# export Robot
-# export split
-# export rotx, roty, rotz
-# export flip
+export Robot
+export split
+export rotx, roty, rotz
+export flip
 export foo
 
 
