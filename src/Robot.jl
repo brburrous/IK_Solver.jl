08a6ac6b032db8b91526aa493b8b2dafff8cbd6e
Brian Burrous <brianon007@gmail.com>
2022-12-30 14:54:28 -0800

    re-added new constructor

--- src/Robot.jl
+++ src/Robot.jl
@@ -10,13 +10,13 @@ end
 # DH table follows format: DH(a, α, d, θ)
 
 # Constructor with standard target matrix
-# function Robot(DH::Array)
-#     A = SymPy.Sym.([
-#     "r₁₁" "r₁₂" "r₁₃" "p_x"
-#     "r₂₁" "r₂₂" "r₂₃" "p_y"
-#     "r₃₁" "r₃₂" "r₃₃" "p_z"
-#     ])
-#     B = [0 0 0 1]
-#     target = vcat(A, B)
-#     Robot(DH, target)
-# end
+function Robot(DH::Array)
+    A = SymPy.Sym.([
+    "r₁₁" "r₁₂" "r₁₃" "p_x"
+    "r₂₁" "r₂₂" "r₂₃" "p_y"
+    "r₃₁" "r₃₂" "r₃₃" "p_z"
+    ])
+    B = [0 0 0 1]
+    target = vcat(A, B)
+    Robot(DH, target)
+end
