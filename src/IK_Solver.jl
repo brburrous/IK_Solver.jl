a1d598499f0a6e0741a7deebf809bcd54b19a2ea
Brian Burrous <brianon007@gmail.com>
2022-12-30 13:44:31 -0800

    test

--- src/IK_Solver.jl
+++ src/IK_Solver.jl
@@ -3,6 +3,7 @@ module IK_Solver
 using SymPy
 using LaTeXStrings
 
+
 include("Robot.jl")
 include("Equation.jl")
 include("HelperFunctions.jl")
@@ -15,6 +16,7 @@ greet() = print("Hello World!")
 export Robot
 export split
 export rotx, roty, rotz
+export flip
 
 
 end # module IK_Solver
